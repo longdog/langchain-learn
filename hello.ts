5d360e1cfdbd966f841fce272c5a00ce4aac3d35
longdog <linda@longdog.us>
2025-08-29 18:34:14 +0400

    human and system prompt

--- hello.ts
+++ hello.ts
@@ -1,10 +1,14 @@
 import {ChatGroq} from "@langchain/groq"
+import {HumanMessage, SystemMessage} from "@langchain/core/messages"
 
 const model = new ChatGroq({
   model: "meta-llama/llama-4-scout-17b-16e-instruct",
   temperature: 0,
 });
-
-const res = await model.invoke("The sky is ")
-console.log(res.result)
+const prompt = [
+  new SystemMessage(`You are a helpful assistant that responds to questions with three exclamation marks.`),
+  new HumanMessage("What is the capital of france?")
+]
+const res = await model.invoke(prompt)
+console.log(res.content)
 
