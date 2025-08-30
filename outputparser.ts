a65a37e87d9a552a453498e7f3d0204686bfcfe7
longdog <linda@longdog.us>
2025-08-30 16:47:42 +0400

    output parser

---
+++ outputparser.ts
@@ -0,0 +1,5 @@
+import { CommaSeparatedListOutputParser } from "@langchain/core/output_parsers";
+
+const parser = new CommaSeparatedListOutputParser();
+const res = await parser.invoke("a,b,c ");
+console.log(res);
